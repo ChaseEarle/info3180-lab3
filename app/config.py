ae256e7231e63f06a328b580491ec7ad58548946
Chase <chasebusbyearle@gmail.com>
2023-02-19 22:29:23 -0500

    final

--- app/config.py
+++ app/config.py
@@ -1,9 +1,15 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = ".env"
+if os.path.isfile(env_path):
+    load_dotenv(dotenv_path=env_path)
 
 class Config(object):
     """Base Config Object"""
-    DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
+    MAIL_PORT = os.environ.get('MAIL_PORT') or '25' or '2525' or '465' or '587'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app import mail
+from flask_mail import Message
+from .forms import ContactForm
 
 
 ###
@@ -56,3 +59,30 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+    """Render the website's contact page."""
+
+    # check that if a POST request is made and that you validate the user input on submit.
+    the_form = ContactForm()
+    if request.method == "POST":
+        if the_form.validate_on_submit():
+            name = the_form.name.data
+            email = the_form.email.data
+            subject = the_form.subject.data
+            text_area = the_form.text_area.data
+
+            msg = Message("Your Subject",
+                sender=("Senders Name", "from@example.com"),
+                recipients=["to@example.com"])
+            msg.body = text_area
+            mail.send(msg)
+
+            flash("Form sucessfully completed!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Please fill out all fields appropriately.")
+            flash_errors(the_form)
+
+    return render_template('contact.html', form=the_form)
